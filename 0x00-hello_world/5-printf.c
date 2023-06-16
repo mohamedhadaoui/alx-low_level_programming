3fb6333bf8b98ed5a06d37ba090a4b60f341a86f
mohamedhadaoui <mohamed.hadaoui20@ump.ac.ma>
2023-06-15 17:51:05 -0700

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - a program that uses the function printf to print a line
+ * Return 0 (success)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
